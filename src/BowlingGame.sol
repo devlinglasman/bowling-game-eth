61e2f0acd976b1040540db0cc0f75dac4bb0969b
Devlin Glasman <devlin.glasman@gmail.com>
2021-11-14 16:04:31 +0000

    add all 20 frames

--- src/BowlingGame.sol
+++ src/BowlingGame.sol
@@ -7,11 +7,32 @@ contract BowlingGame {
     // The game can interrogate the previous frame about whether it is waiting due
     // to a spare or strike, then give the next rolls to it.
 
-    Frame[2] allFrames;
+    Frame[20] allFrames;
     uint256 currentFrameNumber;
 
     constructor() {
-        allFrames = [new Frame(0), new Frame(1)];
+        allFrames = [
+        new Frame(0),
+        new Frame(1),
+        new Frame(2),
+        new Frame(3),
+        new Frame(4),
+        new Frame(5),
+        new Frame(6),
+        new Frame(7),
+        new Frame(8),
+        new Frame(9),
+        new Frame(10),
+        new Frame(11),
+        new Frame(12),
+        new Frame(13),
+        new Frame(14),
+        new Frame(15),
+        new Frame(16),
+        new Frame(17),
+        new Frame(18),
+        new Frame(19)
+        ];
         currentFrameNumber = 0;
     }
 
@@ -23,6 +44,7 @@ contract BowlingGame {
         } else {
             allFrames[currentFrameNumber].setThirdRoll(pinsKnockedDown);
         }
+
         if (isFrameFinished(currentFrameNumber)) {
             currentFrameNumber++;
         }
--- src/test/BowlingGame.t.sol
+++ src/test/BowlingGame.t.sol
@@ -24,20 +24,34 @@ contract TestBowlingGame is BowlingGameTest {
         bowlingGame.roll(1);
         assertTrue(bowlingGame.isFrameFinished(0));
     }
+
+    function test_score_frames_when_no_tens() public {
+        bowlingGame.roll(4);
+        bowlingGame.roll(1);
+        bowlingGame.roll(3);
+        bowlingGame.roll(4);
+
+        uint256 frameScore0 = bowlingGame.scoreForFrame(0);
+        assertEq(frameScore0, 5);
+        uint256 frameScore1 = bowlingGame.scoreForFrame(1);
+        assertEq(frameScore1, 7);
+    }
 //
-//    function test_score_frames_when_no_tens() public {
+//    function test_score_frames_when_first_is_spare() public {
 //        bowlingGame.roll(4);
-//        bowlingGame.roll(1);
-//        uint256 frameScore0 = bowlingGame.scoreForFrame(0);
-//        assertEq(frameScore0, 5);
-//
+//        bowlingGame.roll(6);
 //        bowlingGame.roll(3);
 //        bowlingGame.roll(4);
+//        bowlingGame.roll(2);
+//        bowlingGame.roll(2);
+//
+//        uint256 frameScore0 = bowlingGame.scoreForFrame(0);
+//        assertEq(frameScore0, 5);
 //        uint256 frameScore1 = bowlingGame.scoreForFrame(1);
 //        assertEq(frameScore1, 7);
 //    }
 
-//    function test_score_frame_when_previous_spare() public {
+    //    function test_score_frame_when_previous_spare() public {
 //        bowlingGame.roll(5);
 //        bowlingGame.roll(5);
 //        bowlingGame.roll(5);
